ef4e5f1fd481d85f2d9a06bf22faa554cd5c5e5b
anovazzi1 <otavio2204@gmail.com>
2024-03-21 20:40:28 -0300

    Add operations.c file with music-related functions

---
+++ src/operations.c
@@ -0,0 +1,24 @@
+// Admin only operations
+int cadastrar_musica(int id){
+
+}; // Function to register a song using an id
+int remover_musica(int id){
+
+}; // Function to remove a song given its id
+
+// Any User operations
+int listar_musicas_ano(int ano){
+
+};
+int listar_musicas_idioma_ano(char* idioma, int ano){
+
+}; 
+int listar_musicas_tipo(char* tipo){
+
+};
+int listar_informacoes_musica(int id){
+
+};
+int listar_informacoes_todas_musicas(){
+    
+};
